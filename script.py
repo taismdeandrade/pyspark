d25a72f945e8450271f7ecc791e79433910e86f2
TaisAndrade <taismdeandrade@gmail.com>
2025-06-04 10:46:50 -0300

    feat: adiciona o cast do tipo date

--- script.py
+++ script.py
@@ -9,19 +9,20 @@ spark = (
     .getOrCreate()
 )
 
+#Cria um dataframe chamado "dados" a partir do arquivo csv
 dados = spark.read.csv('amostragem.csv', header=True, inferSchema=True, sep=';')
 
-#df.show()
-#df.printSchema()
+#Mostra os tipos de dados das colunas
+dados.printSchema()
 
 #mostra apenas as colunas selecionadas
 dados.select('Nome da Tarefa', 'Tipo da Tarefa', 'Status Descrição', 'Usuário').show()
 
-#filtra os dados e seleciona apenas os do usuario jeferson klau
+#Cria um novo dataframe chamado usuario filtrando do dataframe dados, seleciona apenas os relacionados ao usuario jeferson klau
 usuario = dados.filter(dados['Usuário'] == 'Jeferson Klau')
 usuario.show()
 
-#Filtra por usuario e descrição concluido
+#Cria um novo dataframe chamado concluido, e filtra por usuario e descrição concluido
 concluido = dados.filter((dados['Usuário'] == 'Jeferson Klau') & (dados['Status Descrição'] == 'Concluído'))
 concluido.show()
 
@@ -29,9 +30,13 @@ concluido.show()
 concluido = usuario.filter(usuario['Status Descrição'] == 'Concluído')
 concluido.show()
 
+#Renomeia as colunas "Nome da Tarefa", "Status", e "Status descrição" para "Nome", "Status Id", e "Status" respectivamente
 dados = dados.withColumnRenamed('Nome da Tarefa', 'Nome').withColumnRenamed('Status', 'Status Id').withColumnRenamed('Status descrição', 'Status')
 
-#transforma os valores da coluna Status de concluído e a fazer para done e todo
+#Muda o tipo da coluna "Data de Criação" de timestamp para o tipo date
+dados = dados.withColumn('Data de Criação', col('Data de Criação').cast('date'))
+
+#transforma os valores da coluna "Status" de "concluído" e "A fazer" para "done" e "todo"
 dados = dados.withColumn('status', regexp_replace('Status', 'Concluído', 'done'))
 dados = dados.withColumn('status', regexp_replace('Status', 'A Fazer', 'todo'))
 dados.show()
