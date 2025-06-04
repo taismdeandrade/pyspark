5cab69ab6db53af4f9ac4c34f9916ace1e0adf5f
TaisAndrade <taismdeandrade@gmail.com>
2025-06-04 08:38:29 -0300

    first commit

---
+++ script.py
@@ -0,0 +1,33 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+spark = (
+    SparkSession.builder
+    .master('local')
+    .appName('dados')
+    .getOrCreate()
+)
+
+dados = spark.read.csv('amostragem.csv', header=True, inferSchema=True, sep=';')
+
+#df.show()
+#df.printSchema()
+
+#mostra apenas as colunas selecionadas
+dados.select('Nome da Tarefa', 'Tipo da Tarefa', 'Status Descrição', 'Usuário').show()
+
+#filtra os dados e seleciona apenas os do usuario jeferson klau
+usuario = dados.filter(dados['Usuário'] == 'Jeferson Klau')
+usuario.show()
+
+#utiliza o data frame usuario e filtra pela descrição concluido
+concluido = usuario.filter(usuario['Status Descrição'] == 'Concluído')
+concluido.show()
+
+dados = dados.withColumnRenamed('Nome da Tarefa', 'Nome').withColumnRenamed('Status', 'Status Id').withColumnRenamed('Status descrição', 'Status')
+
+#transforma os valores da coluna Status de concluído e a fazer para done e todo
+dados = dados.withColumn('status', regexp_replace('Status', 'Concluído', 'done'))
+dados = dados.withColumn('status', regexp_replace('Status', 'A Fazer', 'todo'))
+dados.show()
